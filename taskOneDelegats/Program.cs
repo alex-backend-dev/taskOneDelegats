a85049a49f511b8c52f9d8566fe8baaddcf3a805
Alex Ulyanitskiy <adl821440@gmail.com>
2021-04-18 23:29:18 +0300

    Add project files.

---
+++ taskOneDelegats/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace taskOneDelegats
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Func<Action<float>, int, float, bool> lambda = Function; 
+            Action<float> action = Method; 
+
+            Console.WriteLine(lambda(action, 2, 3.5f));
+            Console.ReadLine();
+        }
+        static void Method(float s)
+        {
+            Console.WriteLine(s);
+        }
+
+        static bool Function(Action<float> action, int integer, float numeric)
+        {
+            float res = numeric % integer;
+            action(res);
+            return res >= 0;
+        }
+    }
+}
+
